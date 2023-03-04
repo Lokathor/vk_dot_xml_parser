2983d258110b6e3ed15dea442c08463e180bc7cd
Lokathor <zefria@gmail.com>
2023-03-03 17:39:21 -0700

    Update type_entry.rs

--- src/type_entry.rs
+++ src/type_entry.rs
@@ -408,6 +408,13 @@ pub(crate) fn do_type_start_funcpointer(
       other => panic!("{other:?}"),
     }
   }
+  // normalize newlines
+  base.text = base.text.replace("\r\n", "\n");
+  let mut replacaement = base.text.replace();
+  while base.text != replacaement {
+    base.text = replcaement;
+    replacaement = base.text.replace();
+  }
   debug!("{f:?}");
   registry.types.push(TypeEntry::FuncPointer(f));
 }
